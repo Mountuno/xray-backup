5b6ebf892b92d3a0aeb174a0d50bf3aed3dd4ad5
Mohammadhossein <Musikreedz@Gmail.com>
2023-06-16 17:40:49 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,26 +1,22 @@
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-
 if [ "$(id -u)" -ne 0 ]; then
-    echo "${RED}This script must be run with root privileges. Please run it as root or using sudo -l."
+    echo "This script must be run with root privileges. Please run it as root or using sudo -l."
     exit 1
 fi
 
-echo "${GREEN}Welcome to ${RED}V2Last ${GREEN}Telegram Backup Helper."
+echo "Welcome to V2Last Telegram Backup Helper."
 echo ""
-echo "${GREEN}__     ______  _              _     ____             _                "
-echo "${GREEN}\ \   / /___ \| |    __ _ ___| |_  | __ )  __ _  ___| | ___   _ _ __  "
-echo "${GREEN} \ \ / /  __) | |   / _` / __/ __/ ]  _/  / _` |/ __| |/ / | | | '_ \ "
-echo "${GREEN}  \ V /  / __/| |__| (_| \__ \ |_  ] |_) | (_| | (__|   <| |_| | |_) |"
-echo "${GREEN}   \_/  |_____|_____\__,_|___/\__| |____/ \__,_|\___|_|\_/___,_| .__/ "
+echo "__     ______  _              _     ____             _                "
+echo "\ \   / /___ \| |    __ _ ___| |_  | __ )  __ _  ___| | ___   _ _ __  "
+echo " \ \ / /  __) | |   / _` / __/ __/ ]  _/  / _` |/ __| |/ / | | | '_ \ "
+echo "  \ V /  / __/| |__| (_| \__ \ |_  ] |_) | (_| | (__|   <| |_| | |_) |"
+echo "   \_/  |_____|_____\__,_|___/\__| |____/ \__,_|\___|_|\_/___,_| .__/ "
 echo "                                                               |_|    "                                                               
 echo ""                                                                                                                              
 echo ""
 echo ""
 sleep 3
-echo "${RED}RUNNING THIS INSTALL SCRIPT WILL REMOVE YOUR PREVIOUS INSTALLATION OF THE SCRIPT"
-read -p "${RED}ARE YOU SURE YOU WANT TO PROCEED? ${GREEN}(y/n): " confirmation
+echo "RUNNING THIS INSTALL SCRIPT WILL REMOVE YOUR PREVIOUS INSTALLATION OF THE SCRIPT"
+read -p "ARE YOU SURE YOU WANT TO PROCEED? (y/n): " confirmation
 
 if [[ $confirmation == [Yy] ]]; then
     echo "Confirmed. Proceeding..."
@@ -30,12 +26,12 @@ else
     exit 3
 fi
 
-echo "${GREEN}Installing necessary packages..."
+echo "Installing necessary packages..."
 apt update && apt upgrade -y
 apt install curl -y
 
-read -p "${GREEN}Please enter your Telegram Bot Token: " setup_token
-read -p "${GREEN}Please enter your Telegram UserID: " setup_chatid
+read -p "Please enter your Telegram Bot Token: " setup_token
+read -p "Please enter your Telegram UserID: " setup_chatid
 
 touch /opt/xray_backup.sh
 script='#!/bin/bash
@@ -66,8 +62,8 @@ else
 fi'
 echo "$script" > /opt/xray_backup.sh
 
-echo "${GREEN}Creating crontab rules..."
+echo "Creating crontab rules..."
 (crontab -u root -l ; echo "0 12 * * * ./opt/xray_backup.sh") | sort - | uniq - | crontab -
 
-echo "${GREEN}The script is installed successfully."
-echo "${GREEN}It will send you the backup files to you via Telegram at 12:00 PM every day."
+echo "The script is installed successfully."
+echo "It will send you the backup files to you via Telegram at 12:00 PM every day."
